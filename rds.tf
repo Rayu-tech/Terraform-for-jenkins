3a17353fd8ec85b3fc43891df6b704bd85c29786
Rayu-tech <rayu30616@gmail.com>
2025-06-23 15:28:05 +0530

    Create rds.tf

---
+++ rds.tf
@@ -0,0 +1,25 @@
+# rds.tf
+resource "aws_db_subnet_group" "rds_subnet_group" {
+  name       = "rds-subnet-group"
+  subnet_ids = [aws_subnet.private_a.id, aws_subnet.private_b.id]
+  tags = {
+    Name = "RDSSubnetGroup"
+  }
+}
+
+resource "aws_db_instance" "db" {
+  allocated_storage      = 20
+  engine                 = "mysql"
+  engine_version         = "8.0"
+  instance_class         = "db.t3.micro"
+  name                   = "mydb"
+  username               = var.db_username
+  password               = var.db_password
+  db_subnet_group_name   = aws_db_subnet_group.rds_subnet_group.name
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  skip_final_snapshot    = true
+  publicly_accessible    = false
+  tags = {
+    Name = "PrivateRDS"
+  }
+}
