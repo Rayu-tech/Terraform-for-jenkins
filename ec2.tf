fe2d93e911c20a465154a92237b4be633c7eab8a
Rayu-tech <rayu30616@gmail.com>
2025-06-23 16:06:30 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 # ec2.tf
 resource "aws_instance" "web" {
-  ami                    = "ami-07caf09b362be10b8"
+  ami                    = "ami-09693150331f14b97"
   instance_type          = "t3.micro"
   subnet_id              = aws_subnet.public_a.id
   vpc_security_group_ids = [aws_security_group.web_sg.id]
